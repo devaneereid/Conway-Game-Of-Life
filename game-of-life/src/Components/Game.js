b4b7080ab93648e0d42458c41fc41a2d15bdf33d
Devanee <devaneereid@gmail.com>
2020-07-27 11:49:18 -0600

    setting up the grid in Game.js

---
+++ game-of-life/src/Components/Game.js
@@ -0,0 +1,123 @@
+import React from 'react';
+
+class Cells extends React.Component {
+    render() {
+        const {x, y} = this.props;
+        return (
+            <div className="Cell" 
+                style={{ 
+                    left: `${CELL_SIZE * x + 1}px`, 
+                    top: `${CELL_SIZE * y + 1}px`,
+                    width: `${CELL_SIZE -1}px`,
+                    height: `${CELL_SIZE - 1}px`}} />
+        );
+    }
+}
+
+const CELL_SIZE = 20;
+const WIDTH = 800;
+const HEIGHT = 600;
+
+class Game extends React.Component {
+    constructor() {
+        super();
+        this.rows = HEIGHT / CELL_SIZE;
+        this.cols = WIDTH / CELL_SIZE;
+        this.board = this.makeEmptyBoard();
+    }
+    state = { 
+        cells: [],
+        interval: 100,
+        isRunning: false,
+    }
+    runGame = () => {
+        this.setState({
+            isRunning: true
+        });
+    }
+    stopGame = () => {
+        this.setState({
+            isRunning: false
+        });
+    }
+    handleIntervalChange = (e) => {
+        this.setState({
+            interval: e.target.value
+        });
+    }
+    // Create an Empty Board
+    makeEmptyBoard() {
+        let board = [];
+        for (let y = 0; y < this.rows; y++) {
+            board[y] = [];
+            for (let x = 0; x < this.cols; x++) {
+                board[y][x] = false;
+            }
+        }
+        return board;
+    }
+    // Create Cells 
+    makeCells() {
+        let cells = [];
+        for (let y = 0; y < this.rows; y++) {
+            for (let x = 0; x < this.cols; x++) {
+                if (this.board[y][x]){
+                    cells.push({ x, y });
+                }
+            }
+        }
+        return cells; 
+    } 
+    // Element offSet - it will calculate the position of the board element
+    getElementOffset() {
+        const rect = this.boardRef.getBoundingClientRect();
+        const doc = document.documentElement;
+        return {
+            x: (rect.left + window.pageXOffset) - doc.clientLeft,
+            y: (rect.top + window.pageYOffset) - doc.clientTop
+        };
+    }
+    // onClick
+    handleClick = (e) => {
+        const elemOffset = this.getElementOffset();
+        const offSetX = e.clientX - elemOffset.x;
+        const offSetY = e.clientY = elemOffset.y;
+        const x = Math.floor(offSetX / CELL_SIZE);
+        const y = Math.floor(offSetY / CELL_SIZE);
+        if ( x >= 0 && x <= this.cols && y >= 0 && y <= this.rows){
+            this.board[y][x] = !this.board[y][x];
+        }
+        this.setState({
+            cells: this.makeCells()
+        });
+    }
+
+    render() {
+        const { cells, isRunning, interval } = this.state;
+        return(
+            <div className="GameBoard">
+                <div 
+                    className="Board" 
+                    style={{ width: WIDTH, height: HEIGHT, backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`}} 
+                    onClick={this.handleClick}
+                    ref={(n) => {this.boardRef = n; }}>
+                        {cells.map(cell => (
+                            <Cells 
+                                x={cell.x} 
+                                y={cell.y} 
+                                key={`${cell.x}, ${cell.y}`} />
+                        ))}
+                </div>
+                <div className="controls">
+                    Update every 
+                    <input value={this.state.interval} 
+                        onChange={this.handleIntervalChange} /> 
+                        msec 
+                        {isRunning ? <button className="button" onClick={this.stopGame}>Stop Game</button> : <button className="button" onClick={this.runGame}>Run Game</button>}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Game;
---
+++ game-of-life/src/Components/Rules.js
@@ -0,0 +1,8 @@
+// In the Game of Life, these rules examine each cell of the grid. For each cell, it counts that cell's eight neighbors (up, down, left, right, and diagonals), and then act on that result.
+
+// * If the cell is alive **and** has 2 or 3 neighbors, then it remains alive. Else it dies.
+// * If the cell is dead **and** has exactly 3 neighbors, then it comes to life. Else if remains dead.
+
+// From those two rules, many types of "creatures" can be created that [move around the "landscape"](https://www.youtube.com/watch?v=28vxPvTDh4E).
+
+// Note: cells that are off the edge of the grid are typically assumed to be dead. (In other cases, people sometimes code it up to wrap around to the far side.)
